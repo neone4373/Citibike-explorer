13053f94383640d82baf5db67e0686439f6b8032
petulla <sam.petulla@gmail.com>
2015-01-08 00:31:50 -0500

    brushing + module patterns

--- scripts/script.js
+++ scripts/script.js
@@ -40,7 +40,7 @@
 			var formatNumber = d3.format(",d"),
       formatChange = d3.format("+,d");
 
-			var dateFormat = d3.time.format("%Y-%m-%d %H:%M:%S");
+			var dateFormat = d3.time.format("%Y-%m-%d %X");
 
 			data.forEach(function(d, i) {
 			  d.index = i;
@@ -65,20 +65,33 @@
 	      all = rides.groupAll(),
 	      /*startTime = rides.dimension(function(d) { return d.starttime; }),
 	      startTimes = startTime.group(d3.time.day),*/
-	      hour = rides.dimension(function(d) { return d.starttime.getHours() + d.starttime.getMinutes() / 60; }),
+	      hour = rides.dimension(function(d) { 
+	      	return (d.starttime.getHours() * 60) + d.starttime.getMinutes(); 
+	      }),
 	      hours = hour.group(Math.floor);
+	      duration =
+	      durations = 
 	      /*delay = flight.dimension(function(d) { return Math.max(-60, Math.min(149, d.delay)); }),
-	      delays = delay.group(function(d) { return Math.floor(d / 10) * 10; }),
-	      distance = flight.dimension(function(d) { return Math.min(1999, d.distance); }),
-	      distances = distance.group(function(d) { return Math.floor(d / 50) * 50; });*/
+	      delays = delay.group(function(d) { return Math.floor(d / 10) * 10; }),*/
+	      duration = rides.dimension(function(d) { return d.tripduration; }),
+	      durations = duration.group(Math.floor);
 	     var charts = [
 
 		    barChart()
 		      .dimension(hour)
 		      .group(hours)
-		    .x(d3.scale.linear()
-		      .domain([0, 24])
-		      .rangeRound([0, 60 * 24])),
+		    	.x(d3.scale.linear()
+		    	//.domain(d3.extent(hours.all()[0]))
+		      .domain([0,1440])
+		      .rangeRound([0, 1000])),
+
+		    barChart()
+		      .dimension(duration)
+		      .group(durations)
+		    	.x(d3.scale.linear()
+		    	//.domain(d3.extent(hours.all()[0]))
+		      .domain([0,5000])
+		      .rangeRound([0, 800])),
 		  ]
 
 		  var chart = d3.selectAll(".chart")
@@ -113,7 +126,7 @@
 		    if (!barChart.id) barChart.id = 0;
 		    var margin = {top: 10, right: 10, bottom: 20, left: 10},
 		      x,
-		      y = d3.scale.linear().range([100, 0]),
+		      y = d3.scale.linear().range([150, 0]),
 		      id = barChart.id++,
 		      axis = d3.svg.axis().orient("bottom"),
 		      brush = d3.svg.brush(),
@@ -126,6 +139,15 @@
 		      var width = x.range()[1],
 		        height = y.range()[0];
 		      y.domain([0, group.top(1)[0].value]);
+
+		      axis
+    			.tickFormat(formatCurrency).tickValues([0,360,720,1080,1440])
+					 function formatCurrency(d) {
+					  
+					  return d/60
+					    
+					}
+		      //console.log(group);
 
 		      div.each(function() {
 		        var div = d3.select(this),
@@ -188,7 +210,7 @@
 		            d;
 		        while (++i < n) {
 		          d = groups[i];
-		          path.push("M", x(d.key), ",", height, "V", y(d.value), "h9V", height);
+		          path.push("M", x(d.key), ",", height, "V", y(d.value), "h.6V", height);
 		        }
 		        return path.join("");
 		      }
@@ -281,7 +303,7 @@
 			interactiveBuilder.dataCF = rides;
 
 			//return ;
-			return console.log(interactiveBuilder.data);
+			return ;
 		},
 		start: function (){
 
@@ -294,4 +316,3 @@
 
 })()
 
-//perform a crossfilter function on the data. use a brush.
